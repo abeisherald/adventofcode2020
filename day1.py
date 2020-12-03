821c0a108f5c90ff1714951cb99f49133b971be1
abeisherald <kinofevil@gmail.com>
2020-12-03 12:36:52 -0500

    part 2

--- day1.py
+++ day1.py
@@ -6,11 +6,18 @@ with open("day1input.txt") as input_list:
     # print(newx)
 
 sum_x = 2020
+incomplete = True
 
-for index, var_a in enumerate(newx):
-    for var_b in newx[(index+1):]:
-        if (var_a + var_b) == sum_x:
-            print(
-                f'variable a is:{var_a} and variable b is:{var_b}. Multiplied against each other results in:{var_a * var_b}')
-        else:
-            continue
+while incomplete:
+    for index, var_a in enumerate(newx):
+        if not incomplete:
+            break
+        for var_b in newx[(index+1):]:
+            if not incomplete:
+                break
+            for var_c in newx[(index+2):]:
+                if (var_a + var_b + var_c) == sum_x:
+                    print(
+                        f'variable a is:{var_a},variable b is:{var_b}, and variable c is:{var_c}. Multiplied against each other results in:{var_a * var_b * var_c}')
+                    incomplete = False
+                    break
