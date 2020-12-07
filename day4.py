5efa3845ccfc6684821ebf5be8ee909b50207a0d
abeisherald <kinofevil@gmail.com>
2020-12-07 18:37:38 -0500

    doesnt work

--- day4.py
+++ day4.py
@@ -1,26 +1,33 @@
-import copy
 
 
 with open('day4input.txt') as passport_batch:
     passport_list = passport_batch.read().splitlines()
 
-fields_list = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid']
 fields_satisfied = {'byr': False, 'iyr': False, 'eyr': False,
-                    'hgt': False, 'hcl': False, 'ecl': False, 'pid': False, 'cid': False}
+                    'hgt': False, 'hcl': False, 'ecl': False, 'pid': False}
 
+# 'cid': False
+
+eligible = 0
 
 for passport_line in passport_list:
-    if passport_line == ' ':
-        determine_eligibility
+    if passport_line == '':
+        if False not in fields_satisfied.values():
+            eligible += 1
+            #print(f'eligible, fields:{fields_satisfied}')
+        else:
+            print(f'ineligible, fields:{fields_satisfied}')
+        fields_satisfied = {
+            field: False for field in fields_satisfied if field != 'cid'}
 
     else:
-        temp = copy.copy(passport_line)
-        while temp != '':
-            field = temp[:(temp.find(' '))]
+        while passport_line != '':
+            space_index = passport_line.find(' ')
+            field = passport_line[:(
+                space_index if space_index != -1 else len(passport_line))]
             field_type = field[:field.find(':')]
             fields_satisfied[field_type] = True
 
-            try:
-                temp = temp[(temp.find(' ')+1):]
-            except:
-                break
+            passport_line = passport_line.replace(field, '').lstrip()
+
+print(eligible)
